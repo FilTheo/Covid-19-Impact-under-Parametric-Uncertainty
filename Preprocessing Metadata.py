1f482c6f98a15b5857e151f951bcb378952f9d11
Filotas Theodosiou <70523417+FilTheo@users.noreply.github.com>
2021-06-15 12:03:18 +0300

    Update Preprocessing Metadata.py

--- Preprocessing Metadata.py
+++ Preprocessing Metadata.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Initial preprocessing techniques are described here.
+#The main objective is to discard papers found in literature which do not include valuable infromation
+#The total length of the dataset will be reduced. 
+#This results in computationaly favourable operations
 
-# In[1]:
-
+#The steps followed in this file include:
+# (1) Keep only papers talking about Covid-19
+# (2) Discard old (before 2019) articles
+# (3) Remove papers with missing infromation (papers with no abstract)
+# (4) Remove non-english papers
+# (5) Make a strong assumption to further reduce the total number of papers
+#     The aim is to make the final dataset computationaly affordable.
 
 import warnings
 warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -31,55 +34,40 @@ import os
 import umap
 
 
-# ### Getting the Metadata csv.
-# 
-# 
-# * Keeping the columns needed
+# Getting the Metadata csv (This includes information regarding the papers found in literature)
+# Keeping the columns needed
 
-# In[2]:
-
-
-#First dataset contains the abstracts and the publish time.
-#Will focus on these!
 metadata = pd.read_csv('D:/complex data project/metadata.csv',
                        usecols=['sha','title','abstract','authors','publish_time'])
 
 
 # Checking the length
+print(len(metadata))
+# Output: 
+# 62330 
 
-# In[3]:
-
-
-len(metadata)
-
+#Considering that 62330 full-text papers will crash the system the aim is to reduce this number
 
 # **Observation**
-# 
-# Several articles are from different times talking about other similar viruses or concepts relevant to COVID-19
-# 
-# The novel coronavirus first appeared in late 2019 and early 2020
-# 
-# Naturally, the first papers were written 
-
-# In[4]:
-
+# Several articles come from different times talking about other similar viruses or concepts relevant to COVID-19
+# The novel coronavirus first appeared in late 2019s and early 2020s
+# Naturally, the first papers were written in that period
 
 #Converting time to datetime
 metadata['publish_time'] = pd.to_datetime(metadata['publish_time'])
 #Extracting just the year from each datetime
 years = [metadata.iloc[i]['publish_time'].year for i in range(len(metadata)) if metadata.iloc[i]['publish_time'].year < 2020 ]
-print(len(years))
+
+#Running a histogram to analyze the distribution of the papers throughout the years
 
 #Plotting
 plt.figure(figsize = (16,8))
 plt.hist(years)
 plt.show()
 
-
-# In[5]:
-
-
-#Keeping only articles from 2020 as I assume that only these articles contain valuable information
+#Keeping only articles from 2020.
+#I assume that only these articles contain valuable information
+#This is not a very strong assumption.
 #There might some articles from past times(like 2019) which give relevant information but in this work there are not included
 def new_articles(date):
     if date.year == 2020:
@@ -91,25 +79,17 @@ metadata['to_keep'] = metadata.apply(lambda row: new_articles(row['publish_time'
 metadata = metadata[metadata['to_keep']==True]
 del metadata["to_keep"]
 
-
-# In[6]:
-
-
 #I have reduced the number to just bellow 190.000
 len(metadata)
+#Output 189304
 
-
-# In[49]:
-
-
-#What is more, several articles talk about pneumonia, sars, or a similar viruses.
-#These articles are not of my concern
-#My Focus is intererely on Covid
-#Again I assume that Covid-relevant info is only on articles containing one of the specific keywords on their abstract(or title)
+#Several articles talk about pneumonia, sars, or a similar viruses.
+#I assume that these articles do not include valuable information
+#My Focus is intererely on Covid-19
+#My hypothesis is that Covid-19 relevant information is included only on articles containing a specific keyword on their abstract(or title)
 #Rarely an article wont include such a term on their abstract(or title)
 
-#Keeping only the articles which contain Covid-19 or sth similar to abstract or title
-
+#Keeping only the articles which contain Covid-19 or a similar keyword or their abstract or title
 #These are the 4 most widely available notations of covid-19
 
 def get_relevant(df):
@@ -120,52 +100,39 @@ def get_relevant(df):
     return(cleaned)
 
 metadata = metadata.fillna('empty')
-#adding the title to the abstract so i wont check both the abstract and the title
+#adding the title to the abstract so i dont have to seperately check both the abstract and the title
 #lowercasing as well
 metadata["check_for_covid"] = metadata["abstract"].str.lower() + metadata["title"].str.lower()
 metadata = get_relevant(metadata)
 metadata = metadata.drop_duplicates(subset='title', keep="first")
 del metadata["check_for_covid"]
 
-
-# In[50]:
-
-
-#As most of my approach will be focused on abstracts
+#A part of my approach is focused on abstracts
+#Observations without abstracts are not usefull
 #I am removing the texts with no abstracts
 
 metadata = metadata[metadata['abstract'] != 'empty']
 len(metadata)
+#Output: 64651
 
+#Next I focus on the language of the papers
+#Most NLP techniques exceel on English language
+#I aim for papers containing english words.
 
-# In[51]:
-
-
-#Next I am going to focus on the language of the papers
-#As my set of techniques focuses on English language
-#I will only focus on papers containing english words.
-
-#To detect the language I will use spacy which is for language processing of scientific articles
+#To extract the language of each paper I use spacy.
+#It is written for language processing of scientific articles
 #Ref: https://spacy.io/universe/project/spacy-langdetect
 
-#Will pick a specific model designed for mining scientific papers
+#Picking a model designed for mining scientific papers
 #The model was selected from:
 #https://allenai.github.io/scispacy/
 #https://github.com/allenai/scispacy
-
-
-# In[52]:
-
 
 #Initializing the pipeline of the used model
 nlp = en_core_sci_lg.load()
 
 #Adding a Language Detector
 nlp.add_pipe(LanguageDetector(), name='language_detector', last=True)
-
-
-# In[53]:
-
 
 #A Small demonstration of the usage of the language detector added on the model's pipeline:
 test = nlp('Hey Everyone')
@@ -174,109 +141,66 @@ if (test1._.language['language'] == "en"):
     print(True, 'It is English')
 else:
     print(test1._.language['language'])
-
-
-# In[54]:
-
+#Output : es
 
 if (test._.language['language'] == "en"):
     print(True)
 else:
     print(test._.language['language'])
-
-
-# In[55]:
-
+#Output: True
 
 def check_language(abstract):
     abstract_nlp = nlp(abstract)
     language = abstract_nlp._.language['language']
     return(language)
 
-#Applying the model on the Abstracts was extremely time-consuming
-#As a result and after checking the perfromance of the language detector
-#Apllying it on the abstracts
+#Applying the model on the Abstracts was time-consuming
+#After considering the perfromance of the language detector on a single sentence:
+#Applying it on the titles
 metadata['Language'] = metadata.apply(lambda row: check_language(str(row['title'])) , axis = 1)
 
-
-# In[56]:
-
-
-#Checking Languages:
+#Checking Language distribtuion
 languages = metadata.groupby('Language').agg('count').reset_index()[['Language','title']].sort_values('title')
 plt.figure(figsize = (12,8))
 plt.barh(languages['Language'],languages['title'])
 plt.show()
 
-
-# In[57]:
-
-
-#English is the most dominant language with some spanish and french articles
-#Keeping only the English Articles
+#English is the most dominant language with a small number of spanish and french articles
+#Keeping only the english articles
 
 metadata = metadata[metadata['Language'] == 'en']
 del metadata['Language']
-len(metadata)
-
-
-# In[58]:
-
-
-#Saving so I wont have to do the same steps again
-
-#import os
-#filepath = 'D:/complex data project'
-#name = os.path.join(filepath , 'metadata1.csv')
-#metadata.to_csv(name)
-
-
-# In[59]:
-
-
-#metadata = pd.read_csv('D:/complex data project/metadata1.csv')
-
-
-# In[60]:
-
 
 #Dropping some duplicates:
 metadata = metadata.drop_duplicates()
 len(metadata)
-
-
-# In[62]:
-
+#Output 62330
 
 #A total of 62.330 Articles!
+#A big dataset concerning the length of each paper (very high dimensional dataset)
 
-#A very big dataset concerning the length of each paper
+#I need to reduce the number of papers even more so the RAM does not crash.
+#I make a strong assumption:
+#Research regarding Covid is assumed to be splitted into two parts!
 
-#My approach to reduce the volume even more is target months
-#Rearch regarding Covid is assumed to be into two parts!
-
-#First part regards the outbreak of the virus in Western World(Italy)
+#First part regards the outbreak of the virus in Western World(Italy) (January)
 #Uncertainty was extremely high till that point and researchers focused on identyfing the mechanisms of the virus
-#This period was until lockdown guidelines were lifted at about May
+#This period lasted until lockdown guidelines were lifted at May (more or less)
 
 #Then the next period is after May where researchers focused on tackling and stopping the spread of the virus
 
-#These assumptions might be uneccessary, but to tackle memory issues raised by my computer(Even Colab or Spark FAILED)
-#I will split the knowledge extractor into two periods.
-#Till May and after May
-
-
-# In[61]:
-
+#These assumptions might be very strong, but to tackle memory issues raised by my computer(Even Colab or Spark FAILED)
+#I will split the dataset into two periods.
+#High uncertainty period (January - May)
+#After-Lockdown period (May - September)
 
 check_dates = [metadata.iloc[i]['publish_time'].month for i in range(len(metadata))]
 plt.figure(figsize=(16,8))
 plt.hist(check_dates)
 plt.show()
 
-
-# In[71]:
-
+#A very big proportion of papers (nearly 30.000) is published on February ( High uncertainty period)
+#After that the papers are evenly distributed across the months
 
 metadata['month_of_publish'] = check_dates
 very_early_papers = metadata[metadata['month_of_publish'] <= 2]
@@ -285,30 +209,18 @@ late_papers = metadata[metadata['month_of_publish'] > 5]
 print("Total of Early Papers:" , len(very_early_papers))
 print("Total of Medium-Period Papers:" , len(mid_period_papers))
 print("Total of Late Papers:" , len(late_papers))
+#Outputs:
+# Total of Early Papers: 33473
+# Total of Medium-Period Papers: 10573
+# Total of Late Papers: 18284
 
-
-# In[72]:
-
-
-#Validating with my primary work
-#metadata1 = pd.read_csv('D:/complex data project/metadata1.csv')
-#metadata1 = metadata1[metadata1['abstract']!='empty']
-#metadata1 = metadata1[metadata1['to_keep']==True].drop_duplicates()
-#metadata1
-
-
-# In[74]:
-
-
-#Some papers had their title as abstract so I am removing them as well!
+# Finaly some papers had their title as their abstract
+# These are also removed as I am interested on the abstracts.
 metadata['title'] = metadata['title'].str.lower()
 metadata['abstract'] = metadata['abstract'].str.lower()
 metadata = metadata[metadata['title']!=metadata['abstract']]
 
-
-# In[77]:
-
-
+#Saving
 filepath = 'D:/complex data project'
 name = os.path.join(filepath , 'step1.csv')
 metadata.to_csv(name)
