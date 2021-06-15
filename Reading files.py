12f00b7f2cdb875b8275cbd461aee3c329e8c5fd
Filotas Theodosiou <70523417+FilTheo@users.noreply.github.com>
2021-06-15 11:13:30 +0300

    Update Reading files.py

--- Reading files.py
+++ Reading files.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Simply reading the json files containing Covid-19 literature (Until Summer 2020)
 
-# In[1]:
-
-
-import numpy as np # linear algebra
+import numpy as np 
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 import json
@@ -12,13 +8,8 @@ from tqdm import tqdm
 import joblib
 
 
-# ### Loading the saved files
-# 
-# Download Link :  https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge
-# 
-
-# In[2]:
-
+# Loading the saved files
+# Download Link :  https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge 
 
 dirs = 'D:/complex data project/pdf_json'
 filenames = os.listdir(dirs)
@@ -27,23 +18,13 @@ print("Number of articles:", len(filenames))
 
 
 # The papers could not be appended on a CSV in a single itteration due to RAM issues
-# 
 # As a result, the following procedure is repeated for a total of 5 times
-# 
-# My idea is to split the files into 5 batches and load 5 difference csv files
-
+# The is to split the files into 5 batches and load 5 difference csv files
 # The following is the procedure for the 4th (out of 5) batch
-
-# In[3]:
-
 
 papers4 = []
 #counting the number of itterations
 splits = int(len(filenames)/5)
-
-
-# In[4]:
-
 
 #1st batch
 
@@ -66,14 +47,8 @@ for i in tqdm(range(4*splits,5*splits)):
 
 
 # **Features Extracted**
-# 
 # The paper_id and the body_text are the only features needed.
-# 
 # The rest are found on the metadata csv (included in the provided link)
-# 
-
-# In[2]:
-
 
 #A function to extract the whole body text:
 
@@ -104,25 +79,18 @@ def get_pd(papers):
     return(full_text_df)
 
 
-# In[7]:
 
 
 #Running
 df5 = get_pd(papers4)
-
-
-# In[8]:
-
 
 #Saving
 filepath = 'D:/complex data project'
 name = os.path.join(filepath , 'paper5.csv')
 df5.to_csv(name)
 
-
-# In[3]:
-
-
+#Repeating for the next batch....
+#Same exact procedure
 dirs = 'D:/complex data project/pmc_json'
 filenames = os.listdir(dirs)
 print("Number of articles:", len(filenames))
